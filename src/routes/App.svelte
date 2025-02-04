80809fdbf7ef500d1487d891220fad557a541134
telmenb <tbayar2@illinois.edu>
2025-02-03 22:01:12 -0600

    progress on key and cell color updating logic

--- src/routes/App.svelte
+++ src/routes/App.svelte
@@ -10,20 +10,23 @@
 	$inspect(currentRow);
 	$inspect(currentCol);
 
-	function keyClicked(key: string) {
+	function keyClicked(key: string): void {
 		if (key === 'enter') {
 			if (currentCol !== wordLength - 1 || board[currentRow][currentCol] === '') {
 				// Display error message
 				return;
 			}
-			currentRow += 1;
-			currentCol = 0;
-			// Check if all cells in row are filled
 			// Check if word exists in word bank
 			//   If word not in word bank, display error message
 			// Post current row to server to check if word is match
+			updateCellAndKeyColors(board[currentRow].join("").toLowerCase(), "уруул"); // let's say API response is lowercase
+			if (board[currentRow].join("").toLowerCase() === "уруул") {
+				// End game
+			}
+			currentCol = 0;
+			currentRow += 1;
 			if (currentRow === wordLength + 1) {
-        // End game
+				// End game
 				return;
 			}
 			return;
@@ -45,6 +48,24 @@
 
 		board[currentRow][currentCol] = key;
 		currentCol += currentCol === wordLength - 1 ? 0 : 1;
+	}
+
+	// TODO: Flesh out this logic
+	function updateCellAndKeyColors(guess: string, answer: string): void {
+		for (let i = 0; i < guess.length; i++) {
+			let cell = document.getElementById(currentRow + '-' + i);
+			let key = document.getElementById(guess[i].toUpperCase());
+			if (guess[i] === answer[i]) {
+				cell?.classList.add('bg-green-300');
+				key?.classList.add('bg-green-300');
+			} else if (answer.includes(guess[i])) {
+				cell?.classList.add('bg-yellow-300');
+				key?.classList.add('bg-yellow-300');
+			} else {
+				cell?.classList.add('bg-gray-500')
+				key?.classList.add('bg-gray-500')
+			}
+		}
 	}
 </script>
 
--- src/routes/Keyboard.svelte
+++ src/routes/Keyboard.svelte
@@ -7,23 +7,22 @@
 		['Я', 'Ч', 'Ё', 'С', 'М', 'И', 'Т', 'Ь', 'В', 'Ю']
 	];
 
-  function onKeyDown(e: KeyboardEvent) {
-    if (e.key === 'Enter') {
-      keyClicked('enter');
-      return;
-    } else if (e.key === 'Backspace') {
-      keyClicked('backspace');
-      return;
-    }
+	function onKeyDown(e: KeyboardEvent) {
+		if (e.key === 'Enter') {
+			keyClicked('enter');
+			return;
+		} else if (e.key === 'Backspace') {
+			keyClicked('backspace');
+			return;
+		}
 
-    if (!keys.some(row => row.includes(e.key.toUpperCase()))) {
-      console.log(`Invalid character ${e.key}`)
-      return;
-    }
+		if (!keys.some((row) => row.includes(e.key.toUpperCase()))) {
+			console.log(`Invalid character ${e.key}`);
+			return;
+		}
 
-    keyClicked(e.key.toUpperCase());
-  }
-  
+		keyClicked(e.key.toUpperCase());
+	}
 </script>
 
 <!-- TODO: Use media queries to make keys smaller on smaller screens -->
@@ -32,20 +31,20 @@
 		<div class="flex {i === 0 ? 'ml-auto pr-8' : ''}">
 			{#if i === keys.length - 1}
 				<button
-					class="m-0.5 h-14 w-8 select-none border border-gray-300 text-center text-xl"
+					class="m-0.5 h-14 w-8 select-none border bg-gray-300 text-center text-xl"
 					onclick={() => keyClicked('backspace')}>⌫</button
 				>
 			{/if}
-			{#each row as key, j}
+			{#each row as key}
 				<button
 					id={key}
-					class="m-0.5 h-14 w-8 select-none border border-gray-300 text-center text-xl"
+					class="m-0.5 h-14 w-8 select-none border bg-gray-300 text-center text-xl"
 					onclick={() => keyClicked(key)}>{key}</button
 				>
 			{/each}
 			{#if i === keys.length - 1}
 				<button
-					class="m-0.5 h-14 w-16 select-none border border-gray-300 text-center text-sm"
+					class="m-0.5 h-14 w-16 select-none border bg-gray-300 text-center text-sm"
 					onclick={() => keyClicked('enter')}>ENTER</button
 				>
 			{/if}
