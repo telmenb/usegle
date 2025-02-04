9874eea2fea9646566624240a4208a62e124a8b6
telmenb <tbayar2@illinois.edu>
2025-02-03 22:34:43 -0600

    improve cell and key color logic

--- src/routes/App.svelte
+++ src/routes/App.svelte
@@ -2,8 +2,14 @@
 	import Board from './Board.svelte';
 	import Keyboard from './Keyboard.svelte';
 
-	const wordLength = 5;
-	let board = $state(new Array(wordLength + 1).fill('').map((_) => new Array(wordLength).fill('')));
+	const WORD_LENGTH = 5;
+	const ANSWER = "хамаг";
+	const CORRECT = "bg-green-300";
+	const INCORRECT = "bg-gray-500";
+	const PARTIAL = "bg-yellow-300";
+	const INACTIVE = "bg-gray-300";
+
+	let board = $state(new Array(WORD_LENGTH + 1).fill('').map((_) => new Array(WORD_LENGTH).fill('')));
 	let currentRow = $state(0);
 	let currentCol = $state(0);
 
@@ -12,60 +18,84 @@
 
 	function keyClicked(key: string): void {
 		if (key === 'enter') {
-			if (currentCol !== wordLength - 1 || board[currentRow][currentCol] === '') {
-				// Display error message
-				return;
-			}
-			// Check if word exists in word bank
-			//   If word not in word bank, display error message
-			// Post current row to server to check if word is match
-			updateCellAndKeyColors(board[currentRow].join("").toLowerCase(), "уруул"); // let's say API response is lowercase
-			if (board[currentRow].join("").toLowerCase() === "уруул") {
-				// End game
-			}
-			currentCol = 0;
-			currentRow += 1;
-			if (currentRow === wordLength + 1) {
-				// End game
-				return;
-			}
+			handleEnterPress();
 			return;
 		}
 
 		if (key === 'backspace') {
-			if (board[currentRow][currentCol] === '' && currentCol !== 0) {
+			handleBackspacePress();
+			return;
+		}
+
+		if (currentCol === WORD_LENGTH - 1 && board[currentRow][currentCol] !== '') {
+			return;
+		}
+
+		board[currentRow][currentCol] = key;
+		currentCol += currentCol === WORD_LENGTH - 1 ? 0 : 1;
+	}
+
+	function handleEnterPress() {
+		if (currentCol !== WORD_LENGTH - 1 || board[currentRow][currentCol] === '') {
+				// Display error message
+				return;
+			}
+
+			// Check if word exists in word bank
+			//   If word not in word bank, display error message
+
+			// Post current row to server to check if word is match
+
+			updateCellAndKeyColors(board[currentRow].join("").toLowerCase(), ANSWER); // let's say API response is lowercase
+			if (board[currentRow].join("").toLowerCase() === ANSWER) {
+				// End game
+			}
+			currentCol = 0;
+			currentRow += 1;
+			if (currentRow === WORD_LENGTH + 1) {
+				// End game
+				return;
+			}
+	}
+
+	function handleBackspacePress() {
+		if (board[currentRow][currentCol] === '' && currentCol !== 0) {
 				currentCol -= 1;
 				board[currentRow][currentCol] = '';
 				return;
 			}
 			board[currentRow][currentCol] = '';
-			return;
-		}
-
-		if (currentCol === wordLength - 1 && board[currentRow][currentCol] !== '') {
-			return;
-		}
-
-		board[currentRow][currentCol] = key;
-		currentCol += currentCol === wordLength - 1 ? 0 : 1;
 	}
 
-	// TODO: Flesh out this logic
 	function updateCellAndKeyColors(guess: string, answer: string): void {
 		for (let i = 0; i < guess.length; i++) {
 			let cell = document.getElementById(currentRow + '-' + i);
 			let key = document.getElementById(guess[i].toUpperCase());
 			if (guess[i] === answer[i]) {
-				cell?.classList.add('bg-green-300');
-				key?.classList.add('bg-green-300');
+				tryUpdateHtmlElementBackgroundColor(cell, CORRECT);
+				tryUpdateHtmlElementBackgroundColor(key, CORRECT);
 			} else if (answer.includes(guess[i])) {
-				cell?.classList.add('bg-yellow-300');
-				key?.classList.add('bg-yellow-300');
+				tryUpdateHtmlElementBackgroundColor(cell, PARTIAL);
+				tryUpdateHtmlElementBackgroundColor(key, PARTIAL);
 			} else {
-				cell?.classList.add('bg-gray-500')
-				key?.classList.add('bg-gray-500')
+				tryUpdateHtmlElementBackgroundColor(cell, INCORRECT);
+				tryUpdateHtmlElementBackgroundColor(key, INCORRECT);
 			}
 		}
+	}
+
+	function tryUpdateHtmlElementBackgroundColor(element: HTMLElement | null, bgColor: string) {
+		element?.classList.remove(INACTIVE);
+		if (bgColor === PARTIAL) {
+			if (!element?.classList.contains(CORRECT)) {
+				element?.classList.add(PARTIAL);
+			}
+			return;
+		}
+		element?.classList.remove(CORRECT);
+		element?.classList.remove(PARTIAL);
+		element?.classList.remove(INCORRECT);
+		element?.classList.add(bgColor);
 	}
 </script>
 
