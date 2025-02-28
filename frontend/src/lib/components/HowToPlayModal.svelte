d50fac4d412e26e580cc979eadfe71bcd0608221
telmenb <tbayar2@illinois.edu>
2025-02-27 21:47:26 -0600

    modal improvements

--- frontend/src/lib/components/HowToPlayModal.svelte
+++ frontend/src/lib/components/HowToPlayModal.svelte
@@ -7,32 +7,20 @@
 </script>
 
 {#if isOpen}
-  <div role="dialog" class="modal">
+  <div role="dialog" class="fixed inset-0 flex justify-center items-center pointer-events-none">
     <div class="contents">
-      <h2>How to Play</h2>
-      <p>Placeholder. Guess the word by typing it out on the keyboard. Press Enter to submit your guess.</p>
-      <div class="actions">
-        <button onclick={() => close()}>OK</button>
+      <div class="flex justify-end">
+        <button onclick={() => close()}>
+          <img src="cross.svg" alt="Close" class="w-8 h-8" />
+        </button>
       </div>
+      <h2 class="text-center text-2xl">Хэрхэн тоглох вэ?</h2>
+      <p class="text=center mt-4">Placeholder. Guess the word by typing it out on the keyboard. Press Enter to submit your guess.</p>
     </div>
   </div>
 {/if}
 
 <style>
-  .modal {
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    right: 0;
-    left: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    /* allow click-through to backdrop */
-    pointer-events: none;
-  }
-
   .contents {
     min-width: 240px;
     max-width: 640px;
@@ -42,23 +30,7 @@
     flex-direction: column;
     justify-content: space-between;
     pointer-events: auto;
+    box-shadow: 0 0 15px rgba(0, 0, 0, 0.15);
+    border-radius: 15px;
   }
-
-  h2 {
-    text-align: center;
-    font-size: 24px;
-  }
-
-  p {
-    text-align: center;
-    margin-top: 16px;
-    border-radius: 6px;
-  }
-
-  .actions {
-    margin-top: 32px;
-    display: flex;
-    justify-content: flex-end;
-  }
-
 </style>
