bd4afdd3ce1d0eef3528bc54810bc62940843dde
telmenb <tbayar2@illinois.edu>
2025-03-03 22:32:39 -0600

    add temp endgame component

---
+++ frontend/src/lib/components/EndGame.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	let { board, won } = $props();
+</script>
+
+<div class="flex flex-col items-center">
+  <h1 class="text-3xl font-semibold mt-8">
+    {#if won}
+      Та амжилттай бөглөлсөн байна!
+    {:else}
+      Даанч буруу бөглөлөө. 😅 Маргааш дахин оролдоорой!
+    {/if}
+  </h1>
+</div>
